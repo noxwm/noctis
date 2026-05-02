0fe86d52cb7d37d3f99d60058f0b3858e88fa99f
notcandy001 <missrishmi@gmail.com>
2026-05-02 12:57:44 +0530

    some of fixeses

---
+++ build/CMakeFiles/noctis.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/noctis.dir/link.d"
+  "CMakeFiles/noctis.dir/protocols/xdg-shell-protocol.c.o"
+  "CMakeFiles/noctis.dir/protocols/xdg-shell-protocol.c.o.d"
+  "CMakeFiles/noctis.dir/src/noctis.c.o"
+  "CMakeFiles/noctis.dir/src/noctis.c.o.d"
+  "noctis"
+  "noctis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/noctis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/noctis.dir/src/noctis.c.o.d
@@ -0,0 +1,97 @@
+CMakeFiles/noctis.dir/src/noctis.c.o: /home/user0/noctis/src/noctis.c \
+ /usr/include/stdc-predef.h /usr/include/assert.h /usr/include/features.h \
+ /usr/include/features-time64.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h /usr/include/getopt.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/getopt_ext.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/stdio.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/stdlib-bsearch.h \
+ /usr/include/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/time.h /usr/include/bits/time.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/unistd_ext.h /usr/include/sys/wait.h \
+ /usr/include/signal.h /usr/include/bits/signum-generic.h \
+ /usr/include/bits/signum-arch.h /usr/include/bits/types/sig_atomic_t.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/siginfo_t.h /usr/include/bits/types/__sigval_t.h \
+ /usr/include/bits/siginfo-arch.h /usr/include/bits/siginfo-consts.h \
+ /usr/include/bits/types/sigevent_t.h /usr/include/bits/sigevent-consts.h \
+ /usr/include/bits/sigaction.h /usr/include/bits/types/stack_t.h \
+ /usr/include/sys/ucontext.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/bits/sigthread.h \
+ /usr/include/bits/signal_ext.h /usr/include/bits/types/idtype_t.h \
+ /usr/include/xkbcommon/xkbcommon.h \
+ /usr/include/xkbcommon/xkbcommon-names.h \
+ /usr/include/xkbcommon/xkbcommon-keysyms.h \
+ /usr/include/xkbcommon/xkbcommon-compat.h \
+ /usr/include/wayland-server-core.h /usr/include/sys/types.h \
+ /usr/include/wayland-util.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h /usr/include/bits/mathcalls-macros.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/inttypes.h \
+ /usr/include/wayland-version.h /usr/include/wlroots-0.18/wlr/backend.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_output.h \
+ /usr/include/pixman-1/pixman.h /usr/include/pixman-1/pixman-version.h \
+ /usr/include/wayland-server-protocol.h /usr/include/wayland-server.h \
+ /usr/include/wayland-server-core.h \
+ /usr/include/wayland-server-protocol.h /usr/include/wayland-util.h \
+ /usr/include/wlroots-0.18/wlr/render/wlr_renderer.h \
+ /usr/include/wlroots-0.18/wlr/render/pass.h \
+ /usr/include/wlroots-0.18/wlr/util/box.h \
+ /usr/include/wlroots-0.18/wlr/render/wlr_texture.h \
+ /usr/include/wlroots-0.18/wlr/render/dmabuf.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_buffer.h \
+ /usr/include/wlroots-0.18/wlr/util/addon.h \
+ /usr/include/wlroots-0.18/wlr/render/allocator.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_compositor.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_cursor.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_output_layout.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_data_device.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_seat.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_input_device.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_keyboard.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_pointer.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_scene.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_damage_ring.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_linux_dmabuf_v1.h \
+ /usr/include/sys/stat.h /usr/include/bits/stat.h \
+ /usr/include/bits/struct_stat.h \
+ /usr/include/wlroots-0.18/wlr/render/drm_format_set.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_subcompositor.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_xcursor_manager.h \
+ /usr/include/wlroots-0.18/wlr/xcursor.h \
+ /usr/include/wlroots-0.18/wlr/util/edges.h \
+ /usr/include/wlroots-0.18/wlr/types/wlr_xdg_shell.h \
+ /home/user0/noctis/protocols/xdg-shell-protocol.h \
+ /usr/include/wayland-server.h /usr/include/wlroots-0.18/wlr/util/log.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h
